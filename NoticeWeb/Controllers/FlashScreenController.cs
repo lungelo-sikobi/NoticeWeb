594bea668a70a05e87f3c49ea620f4a6f6f667cf
masiyahndlovu4 <masiyahndlovu4@gmail.com>
2019-07-17 00:27:54 +0200

    Final treat 16/july/2019

---
+++ NoticeWeb/Controllers/FlashScreenController.cs
@@ -0,0 +1,92 @@
+﻿using Notice.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoticeWeb.Controllers
+{
+    public class FlashScreenController : Controller
+    {
+        // GET: FlashScreen
+        DataAcess dt = new DataAcess();
+        public ActionResult Flashing()
+        {
+            var flash = dt.GetNoticeTitle();
+            return View(flash);
+        }
+
+        // GET: FlashScreen/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FlashScreen/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FlashScreen/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlashScreen/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FlashScreen/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlashScreen/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FlashScreen/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
