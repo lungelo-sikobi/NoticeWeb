69e7503f48e99d1abc405cff70183353e0714c46
Lungelo Sikobi <42305382+Sikobi@users.noreply.github.com>
2019-08-21 19:46:35 +0200

    lungelo

--- NoticeWeb/Controllers/AdminController.cs
+++ NoticeWeb/Controllers/AdminController.cs
@@ -156,27 +156,10 @@ namespace NoticeWeb.Controllers
         //}
 
         // GET: Admin/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+       
 
         // POST: Admin/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
+        
         // GET: Admin/Delete/5
         public ActionResult Delete(int id)
         {
